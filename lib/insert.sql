7a84ef3b2057c2369a0f0ffe225370364bd0d095
Jeremy Garcia <jeremygarcia9400@gmail.com>
2023-08-17 13:48:28 -0400

    complete lab

---
+++ lib/insert.sql
@@ -0,0 +1,15 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Mr. Chocolate', 2, 'M', 'Brown', 'Lazy', True);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Rowdy', 4, 'M', 'Brown', 'Lazy', True);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Tabitha', 7, 'F', 'Brown', 'Lazy', True);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Sergeant Brown', 4, 'M', 'Brown', 'Lazy', True);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Melissa', 5, 'F', 'Brown', 'Lazy', True);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Grinch', 3, 'M', 'Brown', 'Lazy', True);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Wendy', 8, 'F', 'Brown', 'Lazy', True);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, 1, 'M', 'Brown', 'Lazy', True);
